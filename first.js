52c484b30db07f762ad852ed577827f899b9b1c7
garimakhatiwada <garimakhatiwada149@gmail.com>
2026-01-31 15:18:12 +0000

    firstJs

---
+++ first.js
@@ -0,0 +1,31 @@
+console.log("Welcome to javascript");
+name= "Garima Khatiwada";
+console.log(name);
+price=" Rs 100"
+console.log(price)
+x=null;
+console.log(x)
+y=undefined
+console.log(y)
+{
+let a;
+a=2;
+console.log(a)}
+a=4;
+console.log(a)
+const student= {
+  fullName: "Garima Khatiwada",
+  age: 20,
+  cgpa:8.2,
+  bachelorpass: true,
+};
+console.log(student.age);
+console.log(student["fullName"]); 
+student["age"] = student["age"] + 1;
+
+const profile={
+  username: "@garimakhatiwada",
+  isFollow: false,
+  followers: 111,
+  following: 222
+};
---
+++ operators.js
@@ -0,0 +1,30 @@
+// //this is single line comment.
+// /*this is multi-line comment*/ 
+// // Arithmetic operators
+// let a=5;
+// let b=2;
+// console.log("a=",a,"b=",b);
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a**b=",a**b);
+// // unary operators
+// let c=5;
+// let d=8;
+// c=c+1;
+// d=d-1;
+// console.log("c =",c);
+// console.log("d=",d);
+// console.log("c--=",c--);
+// console.log("c=",c);
+// console.log("d++=",d);
+// console.log("a*b=",a*b);
+
+
+let num= prompt("Enter a number");
+if(num%5==0){
+  console.log("Number is a mutiple of 5");
+}
+else{
+  console.log("Number is not a multiple of 5");
+}
