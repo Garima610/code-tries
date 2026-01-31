a7d0a2f164c334add2f0b23fa3af304c672cec42
garimakhatiwada <garimakhatiwada149@gmail.com>
2026-01-31 17:07:08 +0000

    usingConditions

--- operators.js
+++ operators.js
@@ -28,3 +28,26 @@ if(num%5==0){
 else{
   console.log("Number is not a multiple of 5");
 }
+
+
+
+let score= prompt("Enter students score");
+if(score>=80 && score<=100)
+{
+  console.log("grade= A");
+}
+else if(score>=70 && score<80)
+{
+  console.log("grade= B");
+}
+else if(score>=60 && score<70)
+{
+  console.log("grade= C");
+}
+else if(score>=50 && score<60)
+{
+  console.log("grade= D")
+}
+else if(score>=0 && score<50){
+  console.log("grade= F");
+}
