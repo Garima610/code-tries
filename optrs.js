021a9e91241400fd9b94cdf1c5f676b4b44d1b1b
garimakhatiwada <garimakhatiwada149@gmail.com>
2026-06-17 16:39:05 +0100

    operatorss

---
+++ optrs.js
@@ -0,0 +1,19 @@
+let a=5;
+let b=8;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// real-life example
+const product= "shoes";
+let price = "2000";
+let quantity= "4";
+let total_price= price * quantity;
+console.log("Shoes total price is " + total_price);
+
+let SP= 5000;
+let CP= 4000;
+let P= SP - CP;
+console.log("Profit is "+ P);
