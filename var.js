7df2a99843a611b492f57e7b699b767bae34c4e9
garimakhatiwada <garimakhatiwada149@gmail.com>
2026-06-17 16:20:55 +0100

    variables

---
+++ var.js
@@ -0,0 +1,24 @@
+let name = "Garima"
+let age = "25"
+let isadult = true;
+
+console.log(name);
+console.log(isadult);
+console.log(age);
+
+
+let favfood= "momo";
+let favplace= "hills";
+console.log(favfood)
+console.log(favplace)
+
+favfood= "pizza";
+console.log(favfood)
+
+name= "Grisma";
+console.log(name)
+console.log(age + name)
+console.log(name + " " + age)
+
+console.log(name + " " + age + " " + favfood + " " + favplace)
+
